1e92f57b2ab5376ecc36a0c7b9c63f8799faf8ce
Raj <rv2004bolt@gmail.com>
2024-09-03 01:06:25 +0530

    "gagaag"

--- React/userside/src/components/Navbar.jsx
+++ React/userside/src/components/Navbar.jsx
@@ -64,7 +64,7 @@ export default function Navbar() {
            <div className=' flex flex-row gap-2'> 
            
             {/* <Link className="font-medium text-gray-600 hover:text-gray-400 focus:outline-none focus:text-gray-400" to="/wallet"> */}
-           {/* <ConnectWallet/> */}
+           <ConnectWallet/>
             </div>
             <div className=' flex flex-row gap-2'>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file">
